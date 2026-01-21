958983d1ade2da1f3c7805d323a41eb589e04301
Thomas Ralls <tralls.pnw@gmail.com>
2026-01-20 18:05:45 -0800

    Fresh migration

--- apps/cms/src/migrations/index.ts
+++ apps/cms/src/migrations/index.ts
@@ -1,15 +1,9 @@
-import * as migration_20260120_231541 from './20260120_231541';
-import * as migration_20260121_005458 from './20260121_005458';
+import * as migration_20260121_020348 from './20260121_020348';
 
 export const migrations = [
   {
-    up: migration_20260120_231541.up,
-    down: migration_20260120_231541.down,
-    name: '20260120_231541',
-  },
-  {
-    up: migration_20260121_005458.up,
-    down: migration_20260121_005458.down,
-    name: '20260121_005458'
+    up: migration_20260121_020348.up,
+    down: migration_20260121_020348.down,
+    name: '20260121_020348'
   },
 ];
