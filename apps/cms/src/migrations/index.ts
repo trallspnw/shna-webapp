0ccafc1e14571a870a5744d881c232878334d99b
Thomas Ralls <tralls.pnw@gmail.com>
2026-01-20 17:14:12 -0800

    Add media.prefix via migration

--- apps/cms/src/migrations/index.ts
+++ apps/cms/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260120_231541 from './20260120_231541';
+import * as migration_20260121_005458 from './20260121_005458';
 
 export const migrations = [
   {
     up: migration_20260120_231541.up,
     down: migration_20260120_231541.down,
-    name: '20260120_231541'
+    name: '20260120_231541',
+  },
+  {
+    up: migration_20260121_005458.up,
+    down: migration_20260121_005458.down,
+    name: '20260121_005458'
   },
 ];
